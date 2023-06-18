11a023362a46d2ff3f082ce3b361b43556b00fc9
RodrigoPadilha <rodrigo.gts@hotmail.com>
2023-06-18 03:18:43 -0300

    Add variáveis de ambiente em arquivo .env

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
-import { PORT } from "@config/minha-config";
+// import { PORT } from "@config/minha-config";
 
 const app = express();
 
 app.get("/", (req, resp) => {
-  resp.send("Hello World");
+  resp.send(`Hello World ${process.env.DB_HOST}: ${process.env.DB_PORT}`);
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.SERVER_PORT, () => {
   console.log(
-    `Server runnig on http://localhost:${PORT} in ${process.env.NODE_ENV}`
+    `Server runnig on http://localhost:${process.env.SERVER_PORT} 
+    environment: ${process.env.ENVIRONMENT}
+    database: ${process.env.DB_HOST}`
   );
 });
